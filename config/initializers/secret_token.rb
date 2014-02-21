c52809e43258138fc9001351ab1cb1a8e57aa1cb
VictorTpo <victor.thepaut@gmail.com>
2014-02-21 18:46:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statisleague::Application.config.secret_token = 'a11d74a188c408b3aeb12e8aeeef75a505d1a090b58502075debb72f86e594bf10b0926b69b3d4da189666172df27eaa97b9d414ba009d00174ad4cbc9228e8c'
